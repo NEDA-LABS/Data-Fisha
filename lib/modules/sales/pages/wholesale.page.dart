96c65ad08665c4951e79e6ae44b80501eecb7f9b
Eliah7 <eliahmbwilo7@gmail.com>
2020-07-18 16:36:02 +0300

    wholesale and retail products

--- lib/modules/sales/pages/wholesale.page.dart
+++ lib/modules/sales/pages/wholesale.page.dart
@@ -6,29 +6,47 @@ import 'package:smartstock/modules/sales/components/retail.component.dart';
 import 'package:smartstock/modules/sales/states/sales.state.dart';
 
 class WholesalePage extends BFastUIPage {
-   @override
+  @override
   Widget build(var args) {
     return BFastUI.component().consumer<SalesState>((context, salesState) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text("Whole Sale"),
-        ),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.refresh),
-          onPressed:(){salesState.getStockFromRemoteAndStoreInCache();}
-          ),
-        body: GridView.builder(
-            padding: EdgeInsets.all(10),
-            itemCount: salesState.stocks.length,
-            gridDelegate:
-                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-            itemBuilder: (context, index) {
-              return RetailComponents().productCardItem(
-                  productCategory: salesState.stocks[index].productCategory,
-                  productName: salesState.stocks[index].productName,
-                  productPrice: salesState.stocks[index].wholesalePrice);
-            }),
+      return FutureBuilder(
+        future: salesState.loading,
+        
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text("Wholesale"),
+              ),
+              floatingActionButton: FloatingActionButton(
+                  child: Icon(Icons.refresh),
+                  onPressed: () {
+                    salesState.setLoadingFuture(loading: salesState.getStockFromRemoteAndStoreInCache());
+                  }),
+              body: GridView.builder(
+                  padding: EdgeInsets.all(10),
+                  itemCount: salesState.stocks.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2),
+                  itemBuilder: (context, index) {
+                    return RetailComponents().productCardItem(
+                        productCategory:
+                            salesState.stocks[index].productCategory,
+                        productName: salesState.stocks[index].productName,
+                        productPrice:
+                            salesState.stocks[index].wholesalePrice.toString());
+                  }),
+            );
+          } else {
+            return Container(
+              color: Colors.white,
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          }
+        },
       );
     });
-}
+  }
 }
