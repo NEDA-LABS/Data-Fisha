c10411fac5661543e14a57a3618690cfb1f87387
joshuamshana <mama27j@gmail.com>
2022-08-20 13:45:17 +0300

    update text field

--- lib/core/components/text_input.dart
+++ lib/core/components/text_input.dart
@@ -2,13 +2,14 @@ import 'package:flutter/material.dart';
 
 const _b = SizedBox();
 
-textInput({@required Function(String) onText,
-  String initialText = '',
-  String placeholder = '',
-  String label = '',
-  Widget icon = _b,
-  TextInputType type = TextInputType.none,
-  String error = ''}) {
+textInput(
+    {@required Function(String) onText,
+    String initialText = '',
+    String placeholder = '',
+    String label = '',
+    Widget icon = _b,
+    TextInputType type = TextInputType.text,
+    String error = ''}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
@@ -19,43 +20,46 @@ textInput({@required Function(String) onText,
       ),
       Container(
         // height: 40,
-        decoration: const BoxDecoration(
+        decoration: BoxDecoration(
             color: Colors.white,
-            border: Border.fromBorderSide(BorderSide(color: Colors.black)),
-            borderRadius: BorderRadius.all(Radius.circular(3))),
+            border: Border.fromBorderSide(BorderSide(
+                color: error.isNotEmpty
+                    ? Colors.red
+                    : Colors.black)),
+            borderRadius: const BorderRadius.all(Radius.circular(3))),
         child: Row(
           children: [
             Expanded(
                 child: TextField(
-                  controller: TextEditingController(text: initialText),
-                  autofocus: false,
-                  maxLines: 1,
-                  minLines: 1,
-                  onChanged: onText,
-                  keyboardType: type,
-                  decoration: InputDecoration(
-                      border: InputBorder.none,
-                      hintText: placeholder,
-                      contentPadding: const EdgeInsets.all(8)
-                    // labelText: label,
-                    // label: Text(''),
+              controller: TextEditingController(text: initialText),
+              autofocus: false,
+              maxLines: 1,
+              minLines: 1,
+              onChanged: onText,
+              keyboardType: type,
+              decoration: InputDecoration(
+                  border: InputBorder.none,
+                  hintText: placeholder,
+                  contentPadding: const EdgeInsets.all(8)
+                  // labelText: label,
+                  // label: Text(''),
                   ),
-                )),
+            )),
             icon
           ],
         ),
       ),
       error.isNotEmpty && error != null
           ? Padding(
-        padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-        child: Text(
-          error,
-          style: const TextStyle(
-            fontSize: 14,
-            color: Colors.red,
-          ),
-        ),
-      )
+              padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+              child: Text(
+                error,
+                style: const TextStyle(
+                  fontSize: 14,
+                  color: Colors.red,
+                ),
+              ),
+            )
           : _b
     ],
   );
