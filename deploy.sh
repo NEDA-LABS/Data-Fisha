8d14a0bc1b255ad3257048e671490551aa47ef21
joshuamshana <mama27j@gmail.com>
2022-10-11 11:38:56 +0300

    force html rendrer in web

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 rm -r build/web || echo 'Ops!'
-flutter build web
+flutter build web --web-renderer html
 #--web-renderer canvaskit
 firebase deploy --only hosting:edge-smartstock --token="1//03yO_ovTWbbmBCgYIARAAGAMSNwF-L9IrKl4cAocingM3g3J9KTlCU-xAg1oCjReGT9CPISfUIP0-VZfuSOiWN_6Y80qE0lybLzA"
