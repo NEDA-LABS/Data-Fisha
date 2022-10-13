906a3328509a1dd3223131172fda99a173abc9d6
joshuamshana <mama27j@gmail.com>
2022-10-13 23:53:32 +0300

    update deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rm -r build/web || echo 'Ops!'
+#rm -r build/web || echo 'Ops!'
 flutter build web --web-renderer html
 #--web-renderer canvaskit
 firebase deploy --only hosting:edge --token="1//03yO_ovTWbbmBCgYIARAAGAMSNwF-L9IrKl4cAocingM3g3J9KTlCU-xAg1oCjReGT9CPISfUIP0-VZfuSOiWN_6Y80qE0lybLzA"
