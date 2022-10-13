55ebd6ac987bae4f1e362bd88328913e537d39fa
joshuamshana <mama27j@gmail.com>
2022-10-13 19:07:22 +0300

    add build to main domain

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 rm -r build/web || echo 'Ops!'
 flutter build web --web-renderer html
 #--web-renderer canvaskit
-firebase deploy --only hosting:edge-smartstock --token="1//03yO_ovTWbbmBCgYIARAAGAMSNwF-L9IrKl4cAocingM3g3J9KTlCU-xAg1oCjReGT9CPISfUIP0-VZfuSOiWN_6Y80qE0lybLzA"
+firebase deploy --only hosting:edge --token="1//03yO_ovTWbbmBCgYIARAAGAMSNwF-L9IrKl4cAocingM3g3J9KTlCU-xAg1oCjReGT9CPISfUIP0-VZfuSOiWN_6Y80qE0lybLzA"
+firebase deploy --only hosting:web --token="1//03yO_ovTWbbmBCgYIARAAGAMSNwF-L9IrKl4cAocingM3g3J9KTlCU-xAg1oCjReGT9CPISfUIP0-VZfuSOiWN_6Y80qE0lybLzA"
