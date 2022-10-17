641b9b073a260a721ff3790e8ca20b06836a7464
joshuamshana <mama27j@gmail.com>
2022-10-17 16:23:53 +0300

    update version

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #rm -r build/web || echo 'Ops!'
 flutter build web --web-renderer html
 #--web-renderer canvaskit
-firebase deploy --only hosting:edge --token="1//03yO_ovTWbbmBCgYIARAAGAMSNwF-L9IrKl4cAocingM3g3J9KTlCU-xAg1oCjReGT9CPISfUIP0-VZfuSOiWN_6Y80qE0lybLzA"
-firebase deploy --only hosting:web --token="1//03yO_ovTWbbmBCgYIARAAGAMSNwF-L9IrKl4cAocingM3g3J9KTlCU-xAg1oCjReGT9CPISfUIP0-VZfuSOiWN_6Y80qE0lybLzA"
+export FIREBASE_TOKEN="1//03fR_okU7hCZyCgYIARAAGAMSNwF-L9IrfyErR8FogG6g19v9pYs6EJLWKxun9Q5OVHHh89i51XFqvSVgOfZmIVHvuvhsgZULbDw";
+firebase deploy --only hosting:edge --token="$FIREBASE_TOKEN"
+firebase deploy --only hosting:web --token="$FIREBASE_TOKEN"
