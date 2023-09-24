f3a2f356a7edd856af3f29f971cbe50f144744b0
joshuamshana <mama27j@gmail.com>
2023-09-24 19:22:55 +0300

    use production url

--- lib/core/services/util.dart
+++ lib/core/services/util.dart
@@ -6,7 +6,7 @@ import 'package:intl/intl.dart';
 import 'package:smartstock/core/models/menu.dart';
 import 'package:smartstock/core/plugins/js_helper.dart';
 //'http://localhost:3000'; //
-var baseUrl = 'http://localhost:3000';
+var baseUrl = 'https://smartstock-faas.bfast.fahamutech.com';
 
 String shopDatabaseURL(App app) =>
     '$baseUrl/shop/${app.projectId}/${app.applicationId}/v2';
