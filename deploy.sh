d22a1a703f19c4f82d1bd1ae3b3aec4b221de6f0
joshuamshana <joshua.mshana@nmbtz.com>
2023-10-06 09:13:36 +0300

    add alias to firebase config

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 flutter build web --web-renderer html
 #--web-renderer canvaskit
 export FIREBASE_TOKEN="1//03fR_okU7hCZyCgYIARAAGAMSNwF-L9IrfyErR8FogG6g19v9pYs6EJLWKxun9Q5OVHHh89i51XFqvSVgOfZmIVHvuvhsgZULbDw";
+firebase use stable
 firebase deploy --only hosting:edge --token="$FIREBASE_TOKEN"
 firebase deploy --only hosting:web --token="$FIREBASE_TOKEN"
